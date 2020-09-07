4c9c7409d20770503afa904507478fa56ebf9419
Ben Mohammad <benmohammad593@gmail.com>
2020-09-07 09:26:24 +0100

    EditorViewState.java added AutoValue abstract class...

--- app/src/main/java/com/benmohammad/yoyo/main/EditorIntent.java
+++ app/src/main/java/com/benmohammad/yoyo/main/EditorIntent.java
@@ -1,6 +1,9 @@
 package com.benmohammad.yoyo.main;
 
 import com.benmohammad.yoyo.base.MviIntent;
+import com.google.auto.value.AutoValue;
 
 public interface EditorIntent extends MviIntent {
+
+
 }
---
+++ app/src/main/java/com/benmohammad/yoyo/utils/Pair.java
@@ -0,0 +1,14 @@
+package com.benmohammad.yoyo.utils;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+public abstract class Pair<F, S> {
+
+    abstract public F first();
+    abstract public S second();
+
+    public static <A, B> Pair<A, B> create(A a, B b) {
+        return new AutoValue_Pair<>(a, b);
+    }
+}
